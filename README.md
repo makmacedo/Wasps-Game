88638eeb08cb53ff3469a14f9de2911b3c9571e3
Arcadio Macedo <arcmak@gmail.com>
2020-10-11 15:57:58 +0100

    text-only game mode

---
+++ README.md
@@ -0,0 +1,20 @@
+# :bee: Wasps Game :bee:
+
+## :triangular_ruler: Rules
+- In this game 3 types of wasps are generated, each one with its health point and specific damage associated.
+- The player can only attack randomly any living wasp in the nest
+- If the queen is killed all other wasps die.
+- The game ends when there are no wasps left.
+
+## ⚙ Config
+Its possible to config wasp types, numbers, life and damage throug the `nest_config` variable
+
+## ✔ Tests
+There are some tests to assure the right functionality of the game core, due to CORS policy in browser, the `test.js` need to imported via `HTTP` protocol so a local server is recommended! 
+
+Tests results will be printed in the console. :computer:
+
+-----
+
+### :warning: Don't actually kill wasps :bee: 
+wasps are very important to our ecosystems, having a crucial role in the pollination of flowers and fruits! They also can also fight back! soo leave the wasps alone!
---
+++ test.js
@@ -0,0 +1,124 @@
+export var test = function() {
+
+    var test_results = {
+        tests: 0,
+        pass: 0,
+        fail: 0,
+        assertions: 0,
+        errors: []
+    }
+    var nest_config = [
+        {
+            type: 'queen',
+            quantity: 1,
+            hp: 80,
+            damage: 7
+        },
+        {
+            type: 'worker',
+            quantity: 5,
+            hp: 68,
+            damage: 10
+        },
+        {
+            type: 'drone',
+            quantity: 8,
+            hp: 60,
+            damage: 12
+        }
+    ]
+
+ 
+    
+    /**
+     * @test - check all elements are present
+     * 
+     */
+    test_results.tests++
+    let success = true
+
+    document.getElementById('start').click()
+
+    nest_config.forEach(wasp_config => {
+        for (let i = 0; i < wasp_config.quantity; i++) { 
+            let wasp_name = wasp_config.type === 'queen' ? 'queen' : `${wasp_config.type} ${i+ 1}`
+            let wasp_element = document.getElementById(`${wasp_config.type}_${i+ 1}`)
+            test_results.assertions++
+
+            if (!wasp_element.innerText.includes(wasp_name)) {
+                success = false;
+                test_results.errors.push(`failed to assert text ${wasp_name} was present in element ${wasp_element.innerHTML}`)
+            }
+        }
+    })
+
+    if (success) test_results.pass++
+    else test_results.fail++
+
+
+
+    /**
+     * @test - check health points are displayed next to wasp name
+     * 
+     */
+    test_results.tests++
+    success = true
+
+    nest_config.forEach(wasp_config => {
+        for (let i = 0; i < wasp_config.quantity; i++) { 
+            let wasp_element = document.getElementById(`${wasp_config.type}_${i+ 1}`)
+            test_results.assertions++
+
+            if (!wasp_element.innerText.includes(wasp_config.hp)) {
+                success = false;
+                test_results.errors.push(`failed to assert text ${wasp_config.hp} was present in element ${wasp_element.innerHTML}`)
+            }
+        }
+    })
+
+    if (success) test_results.pass++
+    else test_results.fail++
+
+
+
+    /**
+     * @test - its possible to win a game with 12~87 attacks
+     * 
+     */
+    success = false
+    test_results.tests++
+    test_results.assertions++
+    
+    document.getElementById('start').click()
+    var total_wasps = document.querySelectorAll('.wasp').length
+    var dead_wasps = document.querySelectorAll('.dead').length
+
+    for (let i = 0, win = false; (!win) && (i < 87); i++) {
+        if (dead_wasps == total_wasps && (12 < i < 87)) {
+            win = true
+            success = true
+        } else {
+            dead_wasps = document.querySelectorAll('.dead').length
+            setTimeout(function timer() {
+                document.getElementById('strike').click()
+            }, i * 350);
+        }
+    }
+
+    setTimeout(function timer() {
+        if (document.getElementById('winner') != null) {
+            success = true
+        }
+    }, 2000);
+
+    if (success) {
+        test_results.pass++
+    } else {
+        test_results.fail++
+        test_results.errors.push(`failed to assert that is possible to win the game with 12~87 attacks...`)
+    }
+
+
+
+    console.log(test_results)
+}
